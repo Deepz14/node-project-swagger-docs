0d728bac11f0019b44f7ceb8c0b07b5ea086a8aa
iamdeepz <deepakchandrasekaran14@gmail.com>
2022-05-22 17:42:50 +0530

    api resources and swagger docs path updated

--- index.js
+++ index.js
@@ -5,8 +5,27 @@ const yaml = require('yamljs');
 const swaggerDocument = yaml.load('./swagger.yaml');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
+let courses = [
+    {
+        id: 11,
+        name: 'Reactjs',
+        price: '299'
+    },
+    {
+        id: 22,
+        name: 'Angular',
+        price: '499'
+    },
+    {
+        id: 33,
+        name: 'Django',
+        price: '799'
+    }
+]
 
 app.get('/', (req, res) => {
     res.send('<h1> Welcome to the Application! </h1>')
@@ -31,11 +50,31 @@ app.get('/api/facebook/', (req, res) => {
     res.status(200).json(response);
 })
 
+app.get('/api/courseObject', (req, res) => {
+    res.status(200).json({
+        id: 10,
+        name: 'Javascript',
+        price: 399
+    })
+})
+
+app.get('/api/courses', (req, res) => {
+    res.status(200).send(courses)
+})
+
+app.post('/api/courses', (req, res) => {
+    courses.push(req.body);
+    res.send(req.body);
+})
+
+app.get('/api/mycourse/:courseId', (req, res) => {
+    const findCourse = courses.find((course) => course.id === Number(req.params.courseId));
+    res.status(200).send(findCourse);
+})
+
 app.get('/api/:token', (req, res) => {
     const getToken = req.params.token
-    res.status(200).json({
-        token: getToken
-    })
+    res.status(200).send(getToken)
 })
 
 app.listen(PORT, () => {
